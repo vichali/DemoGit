7781c41d873a94fd3fc89b65cef650a4548d05f5
Victor <victorhugo.chalian@gmail.com>
2012-09-05 12:48:53 -0300

    Signed-off-by: Victor <victorhugo.chalian@gmail.com>

--- DemoGIT/src/DemoGit/GitTest.java
+++ DemoGIT/src/DemoGit/GitTest.java
@@ -5,6 +5,7 @@ public class GitTest {
 	public static void main(String[] args) {
 		
 		System.out.println("Ejemplo del uso de GIT");
+		System.out.println("Haciendo cambios en el código");
 		
 	}
 
